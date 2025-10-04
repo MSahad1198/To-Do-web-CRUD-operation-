3b47a649bdfd9fcfb93ed571c4bce527c77be16b
MSahad1198 <mohtasimsahad1198@gmail.com>
2025-10-04 22:07:56 +0600

    Edit task Functionality

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path ('addTask/', views.addTask, name = 'addTask'),
+    
+    #mark done
+    path ('markComplete/<int:pk>/', views.markComplete, name = 'markComplete'),
+    
+    #mark undone
+    path ('Undone/<int:pk>/', views.Undone, name = 'Undone'), 
+
+    #edit feature
+    path ('edit/<int:pk>/', views.editTask, name = 'editTask'),
 ]
